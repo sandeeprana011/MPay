8d7d2c6f71180bf3998a4a16cbbe87658d477231
Sandeep Rana <sandeep.rana@investo2o.com>
2019-10-06 23:10:30 +0530

    Image problem solved

--- MPaySDK/Classes/UserDetailsVC.swift
+++ MPaySDK/Classes/UserDetailsVC.swift
@@ -4,28 +4,43 @@ import WebKit
 public class UserDetailsVC: UIViewController {
     @IBOutlet weak var webview: WKWebView!;
 
-	override public func viewDidLoad() {
+    private var delegateOld: (UIImagePickerControllerDelegate & UINavigationControllerDelegate)?;
+
+    var pickedImage: UIImage?;
+
+    public override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)?) {
+        print("ViewController:", viewControllerToPresent);
+        if let viewControllerPicker = viewControllerToPresent as? UIImagePickerController {
+            self.delegateOld = viewControllerPicker.delegate;
+            viewControllerPicker.delegate = self;
+        }
+		super.present(viewControllerToPresent, animated: flag, completion: completion)
+
+    }
+
+
+    override public func viewDidLoad() {
         super.viewDidLoad();
-        webview.configuration.userContentController.add(self, name: "share");
+        webview.configuration.userContentController.add(self, name: HandlerType.USER_DETAILS.rawValue);
 //		let bundle = Bundle(for: UserDetailsVC.self);
-		let frameworkBundle = Bundle(for: UserDetailsVC.self)
-		let bundleURL = frameworkBundle.resourceURL?.appendingPathComponent("MPaySDK.bundle");
-		let resourceBundle = Bundle(url: bundleURL!);
+        let frameworkBundle = Bundle(for: UserDetailsVC.self)
+        let bundleURL = frameworkBundle.resourceURL?.appendingPathComponent("MPaySDK.bundle");
+        let resourceBundle = Bundle(url: bundleURL!);
 //		let image = UIImage(named: "ic_arrow_back", inBundle: resourceBundle, compatibleWithTraitCollection: nil)
 
-		
+
 //		let bundle = Bundle(identifier: "MPaySDK")
-		let htmlFile = resourceBundle?.path(forResource: "UserDetailsForm", ofType: "html")!;
+        let htmlFile = resourceBundle?.path(forResource: "UserDetailsForm", ofType: "html")!;
         let html = try? String(contentsOfFile: htmlFile!, encoding: .utf8)
         webview.loadHTMLString(html!, baseURL: nil)  // Force unwrapping can be used here
 
     }
-	
-	public static func instantiateVC() -> UserDetailsVC {
-		let storyboard = UIStoryboard(name: "MPay", bundle: Bundle(for: UserDetailsVC.self));
-		let vcUserDetails = storyboard.instantiateInitialViewController();
-		return vcUserDetails as! UserDetailsVC;
-	}
+
+    public static func instantiateVC() -> UserDetailsVC {
+        let storyboard = UIStoryboard(name: "MPay", bundle: Bundle(for: UserDetailsVC.self));
+        let vcUserDetails = storyboard.instantiateInitialViewController();
+        return vcUserDetails as! UserDetailsVC;
+    }
 }
 
 // #MARK: WKScriptMessageHandler
@@ -34,9 +49,42 @@ extension UserDetailsVC: WKScriptMessageHandler {
         switch HandlerType(rawValue: message.name) ?? .NOT_SUPPORTED {
         case HandlerType.USER_DETAILS:
             print("User Details");
+            print(self.pickedImage);
             break;
         default:
             print("handler not supported");
         }
+    }
+}
+
+extension UserDetailsVC: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+	
+	public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		let newInfo = info
+        if let pickedImage = newInfo[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.pickedImage = pickedImage;
+        }
+        self.delegateOld?.imagePickerController!(picker, didFinishPickingMediaWithInfo: info)
+		picker.delegate = self.delegateOld
+	}
+	
+	public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		self.delegateOld?.imagePickerControllerDidCancel?(picker);
+	}
+	
+	
+}
+
+extension String {
+
+    func base64ToImage() -> UIImage? {
+
+        if let decodedData = Data(base64Encoded: self, options: .ignoreUnknownCharacters) {
+            let image = UIImage(data: decodedData)
+            return image;
+        }
+
+        return nil
+
     }
 }
